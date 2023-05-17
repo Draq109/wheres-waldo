cb01c2fa5da4a1fd84bae7c3d8ec1ea006db070f
Draq109 <daniel.vazquez03@utrgv.edu>
2023-05-17 14:51:55 -0500

    Add icons to dropdown content

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import antman from "../images/antman.png"
+import gwen from "../images/gwen.png"
+import deadpool from "../images/deadpool.png"
 
 export default function DropdownMenu() {
     const [active,setActive] = useState(false);
@@ -6,11 +9,11 @@ export default function DropdownMenu() {
 
     return (
         <div className="dropdown">
-            <button className={active ? "dropdown-btn-active" : "dropdown-btn"} onClick={() => active ? setActive(false) : setActive(true)} >...</button>
+            <button type="button" className={active ? "dropdown-btn-active" : "dropdown-btn"} onClick={() => active ? setActive(false) : setActive(true)} >...</button>
             <div className={active ? "dropdown-content-active" : "dropdown-content"} onClick={() => active ? setActive(false) : setActive(true)}>
-                <span className="character-1"><img />char1</span>
-                <span className="character-2"><img />char2</span>
-                <span className="character-3"><img />char3</span>
+                <img className="dropdown-gwen-icon" src={gwen} />
+                <img className="dropdown-deadpool-icon" src={deadpool} />
+                <img className="dropdown-antman-icon" src={antman} />
             </div>
         </div>
     )
